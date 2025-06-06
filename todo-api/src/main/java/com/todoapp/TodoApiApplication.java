01a68b4752d9b8c8f07fd2660cb003eeb46377ed
Muchate <m7chate@gmail.com>
2025-06-06 15:52:57 +0200

    Adiciona TodoApiApplication.java

---
+++ todo-api/src/main/java/com/todoapp/TodoApiApplication.java
@@ -0,0 +1,14 @@
+package com.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class TodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiApplication.class, args);
+    }
+}
